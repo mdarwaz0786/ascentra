1bd8aaa68927b6890727d09b155cf5665ee010f8
Md Arwaz <mdarwaznew2017@gmail.com>
2026-03-12 15:16:52 +0530

    change url

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     host: true,
     proxy: {
-      "/api": "http://localhost:3009",
+      "/api": "https://aceascentra.com",
     },
   },
   plugins: [react()],
